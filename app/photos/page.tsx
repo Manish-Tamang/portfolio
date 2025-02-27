06919411bd7705c7732c14521f2e0222bd983ae3
Manish-Tamang <maneshtamang833@gmail.com>
2025-02-27 13:53:41 +0545

    authjs

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,10 +1,10 @@
+"use client";
+import React, { useState, useRef, useEffect } from "react";
 import { LayoutGrid } from "@/components/ui/layout-grid";
-import SignIn from "@/components/sign-in";
 
 export default function LayoutGridDemo() {
     return (
         <div className="h-screen py-20 w-full">
-            <SignIn />
             <LayoutGrid cards={cards} />
         </div>
     );
