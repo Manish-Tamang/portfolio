cb1cda7b6a37d2bd2293b295ee0c0a0d1d06e653
Manish-Tamang <maneshtamang833@gmail.com>
2025-02-27 23:32:27 +0545

    auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,26 @@
-import { handlers } from "@/auth" 
-export const { GET, POST } = handlers
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+
+const googleId = process.env.AUTH_GOOGLE_ID as string;
+const googleSecret = process.env.AUTH_GOOGLE_SECRET as string;
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: googleId,
+      clientSecret: googleSecret,
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin', 
+  },
+  session: {
+    strategy: "jwt",
+  },
+});
+
+export { handler as GET, handler as POST };
+
+
