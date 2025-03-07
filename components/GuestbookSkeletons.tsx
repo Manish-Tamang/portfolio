248def21523c74528e501e9a4c6bbbcb02c75e7a
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-07 22:29:11 +0545

    Skeleton added

--- components/GuestbookSkeletons.tsx
+++ components/GuestbookSkeletons.tsx
@@ -22,5 +22,8 @@ export const GuestbookSkeletons = () => (
         <GuestbookCardSkeleton />
         <GuestbookCardSkeleton />
         <GuestbookCardSkeleton />
+        <GuestbookCardSkeleton />
+        <GuestbookCardSkeleton />
+        <GuestbookCardSkeleton />
     </>
 );
