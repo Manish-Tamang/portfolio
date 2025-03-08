d6169ce8117e5973d4e8cec56b1d6d7685c0a19a
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-08 19:57:24 +0545

    08/03

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { ArrowUpRight } from "lucide-react";
 import TechStacks from "@/components/TechStacks";
+import dynamic from "next/dynamic";
 
+const HomeScene = dynamic(() => import("@/components/3d-scene"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
@@ -13,8 +18,11 @@ export default function Home() {
             <h1 className="text-3xl md:text-6xl lg:text-5xl font-lifeofapple flex items-center flex-wrap gap-2">
               Hi, I'm
               <div className="inline-flex items-center">
-                <div className="flip-container inline-block" style={{ width: '50px', height: '50px' }}>
-                  <div className="flip-card" style={{ width: '50px', height: '50px' }}>
+                <div
+                  className="flip-container inline-block"
+                  style={{ width: "50px", height: "50px" }}
+                >
+                  <div className="flip-card" style={{ width: "50px", height: "50px" }}>
                     <div className="flip-card-front">
                       <Image
                         alt="Profile Image"
@@ -39,12 +47,8 @@ export default function Home() {
                 </div>
               </div>
               <span className="font-medium">Manish Tamang</span>,
-              <span className="block">
-                developer & creator
-              </span>
-              <span className="">
-                at Swikar Codes.
-              </span>
+              <span className="block">developer & creator</span>
+              <span className="">at Swikar Codes.</span>
             </h1>
             <p className="text-sm md:text-lg text-gray-600 dark:text-gray-300 mt-4 max-w-2xl">
               A wizard who loves development and code. I tell stories through my projects
@@ -56,26 +60,31 @@ export default function Home() {
         <TechStacks />
       </section>
       <section className="mb-16">
-        <h2 className="text-2xl font-medium mb-4">Work</h2>
-        <p className="text-lg mb-12 leading-relaxed max-w-3xl">
+        <h2 className="text-2xl font-peachi font-medium mb-1">Work</h2>
+        <p className="text-lg mb-4 leading-relaxed max-w-3xl">
           I focus on building fast, future-proof solutions that thrive at the intersection of speed and precision.
           Every line of code I write balances rapid execution with meticulous structure, ensuring scalability and
           maintainability. To me, great development means shipping efficiently and leaving a foundation that lasts.
         </p>
-
-        <div className="space-y-8">
+        <div>
+          <Link
+            href="https://staging.swikarcodes.com"
+            className="group flex items-center gap-2 text-xl font-medium underline mb-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Swikar Codes
+            <ArrowUpRight className="w-4 h-4 transition-transform group-hover:translate-x-1 group-hover:-translate-y-1" />
+          </Link>
+          <p className="text-gray-700 dark:text-gray-300">Full Stack Developer</p>
+          <p className="text-gray-600 dark:text-gray-400">Aug. 2024 - current</p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
           <div>
-            <Link
-              href="https://staging.swikarcodes.com"
-              className="group flex items-center gap-2 text-xl font-medium underline mb-2"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Swikar Codes
-              <ArrowUpRight className="w-4 h-4 transition-transform group-hover:translate-x-1 group-hover:-translate-y-1" />
-            </Link>
-            <p className="text-gray-700 dark:text-gray-300">Full Stack Developer</p>
-            <p className="text-gray-600 dark:text-gray-400">Aug. 2024 - current</p>
+            
+          </div>
+          <div>
+            <HomeScene />
           </div>
         </div>
       </section>
