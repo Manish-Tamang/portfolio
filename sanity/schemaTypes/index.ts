d6330ceeed8f1a95e959712c132eaee68f315fef
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-08 19:57:58 +0545

    08/02

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity';
 
 import { post } from './post';
 import { project } from './project';
+import { photo } from './photos';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, project],
+  types: [post, project, photo],
 };
