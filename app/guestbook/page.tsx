37dcf602f5532cd5cd89a27d128e0a7861c92622
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-10 18:57:44 +0545

    Bg color adjustment

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -69,7 +69,7 @@ const GuestbookCardComponent: React.FC<GuestbookCardProps> = ({
     };
 
     return (
-        <div className="rounded-[4px] p-3 w-full border border-gray-200 dark:border-gray-700 shadow-sm mb-2 bg-white dark:bg-gray-900 relative">
+        <div className="rounded-[4px] p-3 w-full border border-gray-200 dark:border-gray-700 shadow-sm mb-2 bg-white dark:bg-[#09090B] relative">
             {isAdmin && (
                 <button
                     onClick={deleteGuestbookEntry}
