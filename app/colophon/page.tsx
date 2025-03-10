f76b25f0818e7063fbfddd463e8a4d77de64e69c
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-10 21:23:46 +0545

    Typography

--- app/colophon/page.tsx
+++ app/colophon/page.tsx
@@ -15,7 +15,9 @@ import {
     SiUmami,
     SiNodedotjs
 } from 'react-icons/si';
-import { FaGit } from "react-icons/fa";
+import { Karla } from 'next/font/google';
+
+const karla = Karla({ subsets: ['latin'] });
 
 const ColophonPage = () => {
     return (
@@ -100,15 +102,16 @@ const ColophonPage = () => {
                 <ul className="list-disc list-inside text-gray-700 dark:text-gray-300 mb-4">
                     <li><b>Geist:</b> Used for the majority of the body content for readability.</li>
                     <li><b>Peachi:</b> Applied to headings to create a distinct and memorable visual hierarchy.</li>
-                    <li><b>Life of Apple:</b>  Elegantly displays my name, lending a personal touch.</li>
+                    <li><b>Life of Apple:</b> Elegantly displays my name, lending a personal touch.</li>
                     <li><b>MonoLisa:</b> Chosen for code snippets, ensuring clarity and legibility.</li>
                     <li><b>Ride My Bike:</b> Font for some special sections of the website.</li>
                 </ul>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <TypographyItem label="Geist Regular" font="'Geist', sans-serif" />
+                    <TypographyItem label="Geist Regular" font="var(--font-geist)" />
+                    <TypographyItem label="Karla Regular" font="'Karla', sans-serif" />
                     <TypographyItem label="Peachi Regular" font="'Peachi', sans-serif" />
-                    <TypographyItem label="Life of Apple Regular" font="'Life of Apple', sans-serif" />
-                    <TypographyItem label="Ride My Bike" font="'Ride My Bike', sans-serif" />
+                    <TypographyItem label="Life of Apple Regular" font="var(--font-lifeofapple)" />
+                    <TypographyItem label="Ride My Bike" font="var(--font-ridemybike)" />
                 </div>
             </section>
 
@@ -131,14 +134,14 @@ const ColophonPage = () => {
                 <ul className="list-disc list-inside text-gray-700 dark:text-gray-300">
                     <li><Link href="https://web3forms.com/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Web3Forms</Link>: For handling form submissions.</li>
                     <li>Weather API: To dynamically display weather information.</li>
-                    <li>Joke API:  To generate random jokes.</li>
+                    <li>Joke API: To generate random jokes.</li>
                     <li>Spotify API: To showcase listening activity.</li>
                     <li>WakaTime API: To display coding statistics.</li>
                     <li><Link href="https://umami.is/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Umami API</Link>: For privacy-focused website analytics.</li>
-                    <li><Link href="https://feedback.fish/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Feedback.fish</Link>:  Providing the feedback widget.</li>
+                    <li><Link href="https://feedback.fish/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Feedback.fish</Link>: Providing the feedback widget.</li>
                     <li><Link href="https://threejs.org/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Three.js</Link> & <Link href="https://docs.pmnd.rs/react-three-fiber/getting-started/introduction" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">React Three Fiber</Link>: For creating interactive 3D elements.</li>
-                    <li><Link href="https://github.com/pmndrs/drei" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">@react-three/drei</Link>:  For useful helpers and abstractions for React Three Fiber.</li>
-                    <li><Link href="https://github.com/pmndrs/fiber" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">@react-three/fiber</Link>:  For React renderer for Threejs.</li>
+                    <li><Link href="https://github.com/pmndrs/drei" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">@react-three/drei</Link>: For useful helpers and abstractions for React Three Fiber.</li>
+                    <li><Link href="https://github.com/pmndrs/fiber" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">@react-three/fiber</Link>: For React renderer for Threejs.</li>
                 </ul>
                 <p className="text-gray-700 dark:text-gray-300 mt-4">
                     The 3D model on the About page is generously provided by <Link href="https://craftz.dog/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Takuya Matsuyama at craftz.dog</Link>.
@@ -152,13 +155,13 @@ const ColophonPage = () => {
                 </p>
                 <ul className="list-disc list-inside text-gray-700 dark:text-gray-300">
                     <li><Link href="https://authjs.dev/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">auth.js v2</Link>: For secure and streamlined user authentication.</li>
-                    <li><Link href="https://github.com/nygardk/react-share" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">react-share</Link>:  To enable easy sharing of content on social media platforms.</li>
+                    <li><Link href="https://github.com/nygardk/react-share" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">react-share</Link>: To enable easy sharing of content on social media platforms.</li>
                     <li>Icons: Sourced from both <Link href="https://react-icons.github.io/react-icons/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">react-icons</Link> and <Link href="https://feathericons.com/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">react-feather</Link>, providing a diverse and consistent icon set.</li>
                     <li><Link href="https://recharts.org/en-US/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">recharts</Link>: Used for data visualization in the dashboard.</li>
                     <li><Link href="https://react-hot-toast.com/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">react-hot-toast</Link>: For displaying user-friendly toast notifications.</li>
                     <li><Link href="https://www.npmjs.com/package/react-canvas-confetti" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">react-canvas-confetti</Link>: To generate engaging confetti effects.</li>
                     <li><Link href="https://custom-cursor.com/" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Custom Cursor</Link>: For unique and personalized custom cursors.</li>
-                    <li><Link href="https://www.npmjs.com/package/react-medium-image-zoom" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">react-medium-image-zoom</Link>:  Enables image zooming functionality.</li>
+                    <li><Link href="https://www.npmjs.com/package/react-medium-image-zoom" className="font-medium text-blue-600 dark:text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">react-medium-image-zoom</Link>: Enables image zooming functionality.</li>
                 </ul>
             </section>
         </div>
