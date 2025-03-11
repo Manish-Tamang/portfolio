94c4fced47973036f5b5c1b859760361f3cfc874
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-11 23:25:35 +0545

    Techstacks

--- components/TechStacks.tsx
+++ components/TechStacks.tsx
@@ -17,7 +17,7 @@ import { FaGit } from "react-icons/fa";
 const TechCard = ({ tech, index }: { tech: any, index: number }) => (
     <div
         key={index}
-        className="flex items-center space-x-3 bg-gray-100 dark:bg-gray-800 px-4 py-2 rounded-xl shadow-sm transition-transform duration-300 hover:scale-105 w-full mr-2 sm:w-48"
+        className="flex items-center space-x-2 bg-gray-100 dark:bg-gray-800 px-2 py-2 rounded-xl shadow-sm transition-transform duration-300 hover:scale-105 w-52 mr-2 sm:w-52"
     >
         <div className="text-xl">{tech.icon}</div>
         <div>
@@ -42,20 +42,20 @@ const TechStacks = () => {
         { icon: <SiPostgresql color="#336791" />, label: "PostgreSQL", desc: "Relational DB" }
     ];
 
-    const firstRow = techStack.slice(0, 5);  // Changed slicing
-    const secondRow = techStack.slice(5); // Changed slicing
+    const firstRow = techStack.slice(0, 5);  
+    const secondRow = techStack.slice(5); 
 
     return (
         <>
             <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
-                <Marquee pauseOnHover className="[--duration:30s] [--gap:0.5rem]">
+                <Marquee pauseOnHover className="[--duration:30s] [--gap:0.2rem]">
                     <div className="flex">
                         {firstRow.map((tech, index) => (
                             <TechCard key={`first-${tech.label}`} tech={tech} index={index} />
                         ))}
                     </div>
                 </Marquee>
-                <Marquee reverse pauseOnHover className="[--duration:30s] [--gap:0.5rem]">
+                <Marquee reverse pauseOnHover className="[--duration:30s] [--gap:0.2rem]">
                     <div className="flex">
                         {secondRow.map((tech, index) => (
                             <TechCard key={`second-${tech.label}`} tech={tech} index={index} />
