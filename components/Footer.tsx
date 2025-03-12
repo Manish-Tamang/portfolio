d5cbecac0370b7ce5da121a714c871fad0954467
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-12 12:39:09 +0545

    mt reduced

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="mt-16 pb-8 rounded-t-3xl pt-10 px-4 sm:px-6 lg:px-8">
+    <footer className="mt-4 pb-8 rounded-t-3xl pt-10 px-4 sm:px-6 lg:px-8">
       <div className="max-w-6xl mx-auto">
         <div className="mb-6">
           <NowPlaying />
