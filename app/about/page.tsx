568d5f50f8d5ae60c3acdb9c5e585c93a3edb5bc
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-12 21:25:21 +0545

    Responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Container from "@/components/Container";
 import { BlurFadeImage, BlurFadeContent } from "@/components/BlurFade";
 import Education from "@/components/Education";
-import ImageGrid from "@/components/ImageGrid";
 import { Dock } from "@/components/Dock";
 import Piano from "@/components/piano/Piano";
-
-
+import ImageGrid from "@/components/ImageGrid";
 
 const About = () => {
+    const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsLargeScreen(window.innerWidth >= 768);
+        };
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+
     return (
         <Container>
             <section className="py-12 md:py-24">
@@ -29,14 +42,11 @@ const About = () => {
                             </span>
                             , where I build the future.
                         </h1>
-                        <p className="text-gray-700 dark:text-gray-300 leading-relaxed mb-6"> I'm a 16-year-old from Itahari,
-                            Nepal, driven by a fervent passion for web development. My coding journey began at a young age, and
-                            since then, I've immersed myself in building captivating websites, integrating and customizing various
-                            technologies like React, Next.js, Tailwind CSS, Postgresql, and more.
+                        <p className="text-gray-700 dark:text-gray-300 leading-relaxed mb-6">
+                            I'm a 16-year-old from Itahari, Nepal, driven by a fervent passion for web development. My coding journey began at a young age, and since then, I've immersed myself in building captivating websites, integrating and customizing various technologies like React, Next.js, Tailwind CSS, PostgreSQL, and more.
                         </p>
-                        <p className="text-gray-700 dark:text-gray-300 leading-relaxed mb-6"> I strongly believe in continuous
-                            learning and improving myself, so I try my best to learn in any situation possible,
-                            unfavorable or not.
+                        <p className="text-gray-700 dark:text-gray-300 leading-relaxed mb-6">
+                            I strongly believe in continuous learning and improving myself, so I try my best to learn in any situation possible, unfavorable or not.
                         </p>
                         <blockquote className="border-l-4 border-gray-300 dark:border-gray-600 pl-4 italic my-6 text-gray-600 dark:text-gray-400">
                             म आफुलाइ राम्रो ठान्छु तर म भन्दा ठूलो पापी छैन, <br />
@@ -44,45 +54,47 @@ const About = () => {
                             हे भग्वान समये बित्यो ऐले म सारै लज्जित छु, <br />
                             पस्चताप को आगो ले म निकै त्रसित छु
                         </blockquote>
-                        {/* <Dock /> */}
                     </div>
-                    <div className="flex flex-col items-center top-40 self-start">
-                        <div className="relative rounded-md w-48 h-full">
-                            <div className="relative group duration-500 cursor-pointer overflow-hidden text-gray-50 h-72 w-56 rounded-2xl hover:duration-700">
-                                <div className="w-56 h-72 text-gray-800">
-                                    <div className="flex flex-row justify-between opacity-90">
-                                        <Image
-                                            src="/img/manish.png"
-                                            alt="Manish Tamang"
-                                            width={200}
-                                            height={200}
-                                            className="w-full h-full"
-                                        />
+
+                    {/* Show the hover image only on larger screens */}
+                    {isLargeScreen && (
+                        <div className="flex flex-col items-center top-40 self-start">
+                            <div className="relative rounded-md w-48 h-full">
+                                <div className="relative group duration-500 cursor-pointer overflow-hidden text-gray-50 h-72 w-56 rounded-2xl hover:duration-700">
+                                    <div className="w-56 h-72 text-gray-800">
+                                        <div className="flex flex-row justify-between opacity-90">
+                                            <Image
+                                                src="/img/manish.png"
+                                                alt="Manish Tamang"
+                                                width={200}
+                                                height={200}
+                                                className="w-full h-full"
+                                            />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="absolute bg-gray-50 dark:bg-gray-800 -bottom-24 w-56 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500">
-                                    <span className="text-[#38A662] font-bold text-xs">Hover here</span>
-                                    <span className="text-[#38A662] font-bold font-peachi text-3xl">Manish Gole</span>
-                                    <p className="text-neutral-800 dark:text-neutral-100">My friends also call me Gole.</p>
+                                    <div className="absolute bg-gray-50 dark:bg-gray-800 -bottom-24 w-56 p-3 flex flex-col gap-1 group-hover:-bottom-0 group-hover:duration-600 duration-500">
+                                        <span className="text-[#38A662] font-bold text-xs">Hover here</span>
+                                        <span className="text-[#38A662] font-bold font-peachi text-3xl">Manish Gole</span>
+                                        <p className="text-neutral-800 dark:text-neutral-100">My friends also call me Gole.</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    )}
+
                     <div className="md:col-span-2">
-                        <ImageGrid />
+                        {/* Show ImageGrid only on larger screens */}
+                        {isLargeScreen && <ImageGrid />}
+
                         <h2 className="text-2xl font-semibold mb-4 font-peachi mt-4">What is My Ambition?</h2>
                         <p className="text-gray-700 font-inter dark:text-gray-300 leading-relaxed mb-6">
-                            My ambition is to grow into a proficient full-stack developer, mastering both
-                            front-end and back-end development to craft powerful, versatile web applications.
-                            Each <a href="#" className="text-[#38A662] hover:underline">project</a> is an opportunity to refine my skills, expand my knowledge, and adapt
-                            to the ever-evolving world of technology. I’m dedicated to making a meaningful impact
-                            through innovation and creativity, one project at a time. A passionate and amateur computer
-                            science enthusiast, I have a deep love for technology and innovation. 🚀
+                            My ambition is to grow into a proficient full-stack developer, mastering both front-end and back-end development to craft powerful, versatile web applications.
+                            Each <a href="#" className="text-[#38A662] hover:underline">project</a> is an opportunity to refine my skills, expand my knowledge, and adapt to the ever-evolving world of technology. I’m dedicated to making a meaningful impact through innovation and creativity, one project at a time. A passionate and amateur computer science enthusiast, I have a deep love for technology and innovation. 🚀
                         </p>
                         <div className="relative rounded-lg overflow-hidden mb-6">
                             <BlurFadeImage
                                 src="/IMG-20250217-WA0011.jpg"
-                                alt="Manish Tamang in Sushma Godawari Collage"
+                                alt="Manish Tamang in Sushma Godawari College"
                                 delay={0.3}
                                 className="object-cover w-full h-full"
                                 width={800}
@@ -109,7 +121,7 @@ const About = () => {
 
                         <h2 className="text-2xl font-semibold mt-10 mb-4 font-peachi">Education</h2>
                         <p className="text-gray-700 dark:text-gray-300 leading-relaxed mb-6">
-                            Here's little <a href="/photos" className="text-[#38A662] hover:underline">photo </a>snippet and timeline of my education
+                            Here's a little <a href="/photos" className="text-[#38A662] hover:underline">photo</a> snippet and timeline of my education.
                         </p>
                         <Education />
                         <div className="relative rounded-lg overflow-hidden mb-6">
@@ -129,12 +141,13 @@ const About = () => {
                         />
                     </div>
                 </div>
-                <div className="mt-4" >
+
+                <div className="mt-4">
                     <h2 className="text-2xl font-peachi font-medium mb-4">Piano</h2>
                     <Piano />
                 </div>
             </section>
-        </Container >
+        </Container>
     );
 };
 
