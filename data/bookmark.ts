e5edf79be7df1c8afff67d00bd45518a7e448465
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-13 12:51:02 +0545

    Bookmark data

---
+++ data/bookmark.ts
@@ -0,0 +1,26 @@
+const bookmarks = [
+  {
+    url: "https://canva.com",
+    icon: "https://img.icons8.com/?size=512&id=iWw83PVcBpLw&format=png",
+    title: "Photo Editor",
+    description:
+      "Online tool to create graphics, presentations, posters, videos",
+  },
+  {
+    url: "https://watermarkremover.io",
+    title: "Watermark Remover",
+    description: "Remove watermark from any image with just one click",
+  },
+  {
+    url: "https://clipdrop.co",
+    title: "Image Upscaler",
+    description: "Upscale, denoise and enhance your images in seconds",
+  },
+  {
+    url: "https://replicate.com",
+    title: "Image Restoration",
+    description: "Practical algorithms for real-world face restoration",
+  },
+];
+
+export default bookmarks;
