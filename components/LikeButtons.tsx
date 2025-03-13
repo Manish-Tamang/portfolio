d23f36c650d1becdf9606770aa7e2b7216cf2d77
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-13 17:25:26 +0545

    Padding added

--- components/LikeButtons.tsx
+++ components/LikeButtons.tsx
@@ -101,7 +101,7 @@ const LikeButtons: React.FC<LikeButtonsProps> = ({ slug }) => {
     };
 
     return (
-        <div className="flex justify-end gap-2 mb-6 p-2 bg-gray-50 dark:bg-[#09090B] rounded-xl">
+        <div className="flex justify-end gap-2 mt-5 mb-6 p-2 bg-gray-50 dark:bg-[#09090B] rounded-xl">
             <button
                 className={`inline-flex items-center justify-center px-3.5 py-2 rounded-full bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-200 text-base font-medium transition-all duration-300 shadow-sm hover:bg-gray-100 dark:hover:bg-gray-700 hover:-translate-y-px ${activeButton === 'thumbsUp' ? 'bg-gray-200 dark:bg-gray-700 shadow-md' : ''}`}
                 onClick={() => handleButtonClick('thumbsUp')}
