f9ac9ae6cbb4a4c128bee7fee9d54238fc9128d2
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-25 12:00:00 +0545

    Timeline

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,9 +1,10 @@
-import { type SchemaTypeDefinition } from 'sanity';
+import { type SchemaTypeDefinition } from "sanity";
 
-import { post } from './post';
-import { project } from './project';
-import { photo } from './photos';
+import { post } from "./post";
+import { project } from "./project";
+import { photo } from "./photos";
+import { timeline } from "./timeline";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, project, photo],
+  types: [post, project, photo, timeline],
 };
