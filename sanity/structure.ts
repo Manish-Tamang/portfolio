a7ad16d81628635dfc7a026ba5c90c3467052b3c
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-26 12:00:00 +0545

    Sanity CMS Structure

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,22 +1,31 @@
-import type { StructureResolver } from 'sanity/structure';
-import { Rss, Code } from 'lucide-react'; // Import Lucide icons
+import type { StructureResolver } from "sanity/structure";
+import { Rss, Code, Image, Clock } from "lucide-react"; 
 
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content Management')
+    .title("Content Management")
     .items([
-      S.listItem()
-        .title('Posts')
-        .icon(Rss) 
-        .child(S.documentTypeList('post')),
+      S.listItem().title("Posts").icon(Rss).child(S.documentTypeList("post")),
 
       S.listItem()
-        .title('Projects')
-        .icon(Code) 
-        .child(S.documentTypeList('project')),
-        
+        .title("Projects")
+        .icon(Code)
+        .child(S.documentTypeList("project")),
+
+      S.listItem()
+        .title("Photos")
+        .icon(Image) 
+        .child(S.documentTypeList("photo")),
+
+      S.listItem()
+        .title("Timeline")
+        .icon(Clock)
+        .child(S.documentTypeList("timeline")),
+
       ...S.documentTypeListItems().filter(
-        (listItem) => !['post', 'project'].includes(listItem.getId() ?? '')
-      ), 
-
+        (listItem) =>
+          !["post", "project", "photo", "timeline"].includes(
+            listItem.getId() ?? ""
+          )
+      ),
     ]);
