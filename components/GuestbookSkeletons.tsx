b7baf5cbf301c503ca307b514fe8a2ebd0b848f2
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-28 18:19:07 +0545

    neutral color added

--- components/GuestbookSkeletons.tsx
+++ components/GuestbookSkeletons.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const GuestbookCardSkeleton = () => {
     return (
-        <div className="rounded-lg p-4 w-full border border-gray-200 dark:border-gray-700 shadow-sm mb-2 bg-white dark:bg-gray-900">
+        <div className="rounded-lg p-4 w-full border border-gray-200 dark:border-gray-700 shadow-sm mb-2 bg-white dark:bg-neutral-900">
             <div className="flex items-start gap-3">
                 <Skeleton className="h-8 w-8 rounded-full" />
                 <div className="flex flex-col flex-1">
