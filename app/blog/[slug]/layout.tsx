48281b0158a5fa7b01cedb15fda0fbe03fecde95
Manish-Tamang <maneshtamang833@gmail.com>
2025-07-13 21:31:26 +0545

    blog layout fixed

--- app/blog/[slug]/layout.tsx
+++ app/blog/[slug]/layout.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function BlogPostLayout({ children }: Props) {
   return (
-    <section className="container mx-auto">
+    <section className="w-full max-w-full sm:max-w-2xl md:max-w-3xl mx-auto px-1 sm:px-2 md:px-6">
       {children}
       <Comments />
     </section>
