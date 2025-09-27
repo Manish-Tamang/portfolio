5aca8772af3f51fe69c09e6feaf234e1aa1d82bb
Manish-Tamang <maneshtamang833@gmail.com>
2025-09-27 22:02:34 +0545

    fix(auth): remove callback URL from logout to fix production logout issue

--- components/AuthButtons.tsx
+++ components/AuthButtons.tsx
@@ -10,7 +10,7 @@ const AuthButtons: React.FC<AuthButtonsProps> = ({ session }) => {
     return (
         <>
             {session?.user && (
-                <Button type="button" className="hover:text-red-600" onClick={() => signOut({ callbackUrl: "/guestbook" })}>
+                <Button type="button" className="hover:text-red-600" onClick={() => signOut()}>
                     Logout
                 </Button>
             )}
