6538b9bd9efd1237419140b2c948e6c47d4f953f
Manish-Tamang <maneshtamang833@gmail.com>
2025-11-22 20:38:20 +0545

    refactor: remove session and callback configurations from NextAuth for simplified authentication setup

--- auth.ts
+++ auth.ts
@@ -4,22 +4,4 @@ import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google, GitHub],
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token && session.user) {
-        session.user.id = token.id as string;
-      }
-      return session;
-    },
-  },
 });
